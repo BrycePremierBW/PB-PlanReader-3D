85a5b02a3bc36fc36747501604e76d71f5c9c446
BrycePremierBW <bryce@premierbw.com.au>
2026-08-23 04:50:41 +1000

    Bump production PlanReader to v1.4.5

--- pb_planreader_v133_app.py
+++ pb_planreader_v133_app.py
@@ -7,7 +7,7 @@ from pb_guided_workflow_v144 import apply as apply_guided_workflow_v144
 app = base.base.launcher.app
 apply_simple_ui_v133(app)
 apply_guided_workflow_v144(app)
-app.APP_VERSION = "1.4.4"
+app.APP_VERSION = "1.4.5"
 
 if __name__ == "__main__":
     app.main()
